04791797c954d7b424d88300729fb3d535972fba
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-09-26 10:04:19 +0200

    26.09

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -31,13 +31,14 @@ export default function Examples() {
   }
     return(
         <Section title="Examples" id="examples">
-            <Tabs buttons={<> <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+            <Tabs
+             buttons={ <> <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
           <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
           <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton></>}>
+          <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton> </>}>
                 {tabContent}
             </Tabs>
-        {tabContent}
+        
       </Section>
     )
 }
